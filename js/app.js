5efe93cece12e3846b33e944b798d25b0715cc7f
stefanom13 <stefanomancuso13@gmail.com>
2022-02-12 17:20:02 +0100

    modifica html e inserimento controllo img js

--- js/app.js
+++ js/app.js
@@ -9,6 +9,7 @@ const items = [
 ];
 
 const title = [
+
     'Svezia',
     'Svizzera',
     'Gran Bretagna',
@@ -18,8 +19,88 @@ const title = [
 
 const text = [
     'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
-    'Lorem ipsum',
+    'Lorem ipsum dolor sit amet consectetur adipisicing elit',
     'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
     'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
     'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
 ]
+// dichiarazioni variabili 
+
+const slideWrapper = document.querySelector('.slide-wrapper')
+console.log(slideWrapper)
+
+const controlImage = document.querySelector('.secondaryImages')
+console.log(controlImage)
+
+const buttSucc = document.querySelector('.up')
+console.log(buttSucc)
+
+const buttPrec = document.querySelector('.down')
+console.log(buttPrec)
+
+let currentIndex = 0
+
+// generare le slide con le immagini 
+// creare ciclio array di img
+    for (let i = 0; i < items.length; i++ ){
+     // creare elemeno html della slide 
+      const item = `
+        <div class="item">
+            <img src="${items[i]}" alt="">
+            <div class="description">
+            <h5 class="titolo">${title[i]}</h5>
+            <p class="paragrafo">${text[i]}</p>
+            </div>
+        </div>
+        ` 
+      slideWrapper.innerHTML += item
+    }
+    const itemImage = [...document.getElementsByClassName('item')]
+    itemImage[currentIndex].classList.add('active')
+
+     // creare click down 
+
+    buttPrec.addEventListener('click', function(){
+        itemImage[currentIndex].classList.remove('active')
+       
+        if( currentIndex > 0){
+          currentIndex--
+          itemImage[currentIndex].classList.add('active')
+         
+        } else {
+          currentIndex = items.length - 1
+        }
+        itemImage[currentIndex].classList.add('active')
+        
+       
+    })
+
+     // creare click up
+    buttSucc.addEventListener('click', function(){
+        itemImage[currentIndex].classList.remove('active')
+        
+        if (currentIndex < items.length - 1){
+          currentIndex++
+          itemImage[currentIndex].classList.add('active')
+
+        } else {
+          currentIndex = 0
+        }
+        itemImage[currentIndex].classList.add('active')
+
+    })
+    // creazione e controllo box img 
+    for (let i=0; i < items.length; i++){
+        const boxImage = `
+        <div class="controls ">
+        <img class="image" src="${items[i]}" alt="">
+        </div>  
+        `  
+        controlImage.innerHTML += boxImage
+      }  
+   
+
+
+
+
+
